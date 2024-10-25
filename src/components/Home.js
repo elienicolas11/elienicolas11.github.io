1789a940b089317047e9d9e065777e28a06106de
Admin <elie.nicolas@etu.univ-amu.fr>
2024-10-25 21:49:32 +0200

    First commit- Project setup

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'C:/Users/Admin/Desktop/React/Portfolio/src/styles/Home.css';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <h1 className="home-title">Welcome to My Home Page</h1>
+      <p className="home-description">This is the main page of the application.</p>
+    </div>
+  );
+};
+
+export default Home;
