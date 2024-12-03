a1dc3dc17adf73dec013e865c3ca335302d9571b
Admin <elie.nicolas@etu.univ-amu.fr>
2024-12-03 13:53:38 +0100

    Modify the favicon

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,8 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
-      filename: 'index.html'
+      filename: 'index.html',
+      favicon: './src/assets/images/favicon.png', 
     })
   ],
   devServer: {
